59f024754f4e4cedbcba9b8bdd07f55b26abc0d6
Alex Lima Lopes Cancado <alxcancado@alxcancados-macbook.local>
2015-08-07 18:07:26 +1200

    Initial Commit

---
+++ Quizzard Watch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quizzard Watch
+//
+//  Created by Alex Lima Lopes Cancado on 7/08/15.
+//  Copyright © 2015 Alex Lima Lopes Cancado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
